17d33d20ff265cf12c817336f5d1eebae2fd8eb1
maryson gleison <maryson011@gmail.com>
2024-07-17 14:11:40 -0300

    feat - configuração do express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-const a = 1
-const b = 3
+import dotenv from 'dotenv'
+dotenv.config()
 
-console.log(a + b)
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const port = process.env.PORTA ?? 3001
+app.listen(port, () => {
+    console.log(`Server is running in port ${port}`)
+})
